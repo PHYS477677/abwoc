387067263c600741af540978e4f410c413c1055c
therealcmeng <78132136+therealcmeng@users.noreply.github.com>
2021-04-08 19:31:28 -0500

    Update data_reducer.py

--- Code/data_reducer.py
+++ Code/data_reducer.py
@@ -1,9 +1,21 @@
 # -*- coding: utf-8 -*-
 """data_reducer.py
 
-This script is designed to be run within a Colab notebook.
-This script reduces full size (4096x4096) .fits images, downscales them
+This script is designed to be run within a pre-mounted Colab notebook.
+This script reduces full size (4096x4096) .fits images, downscales them,
+and then uploads them back into the google drive as .gz files.
 
+To run within a colab script, first "import data_reducer", then execute
+data_reducer.reduce_data(wavelengths, date_paths, reduction_factor)
+
+
+Inputs
+------
+    wavelengths : 1-D list or numpy array of integer wavelengths in Angstroms
+                      over which to reduce.
+    date_paths  : 1-D list of strings in the format "[year]_[month]_"
+                      corresponding to dates over which to reduce.
+    reduction_factor  : integer over which to reduce
 
 
 Original file is located at
@@ -29,10 +41,8 @@ import sunpy.map
 
 
 # =============================================================================
-#                                   Functions
+#                               Helper Functions
 # =============================================================================
-
-
 def mean_pool(square_array, ratio):
     """
     Function to downsample a square array after applying a meanpool.
@@ -40,7 +50,7 @@ def mean_pool(square_array, ratio):
     Inputs
     ------
         square_array : Array to be downsampled. Must be a square array with
-                        axis lengths that can be divisible by ratio
+                       axis lengths that can be divisible by ratio
         ratio        : Downsampling ratio. i.e. a 1024x1024 array with a ratio
                        of 4 will be downsampled to 256x256
 
@@ -89,84 +99,137 @@ def convert_to_gz(fits_folder, gz_folder, down_sample_factor, date,
     gz_list = glob.glob(gz_folder+'*'+date+'*.gz')
     num_gz = len(gz_list)
 
-    total_files = num_fits - num_gz #(total number of files remaining)
-    print("%d fits files in "%num_fits+fits_folder)
-    print("%d gz files in "%num_gz+gz_folder)
+    # (total number of files remaining)
+    total_files = num_fits - num_gz
+    print("%d fits files in " % num_fits + fits_folder)
+    print("%d gz files in " % num_gz+gz_folder)
     if total_files < 1:
-      print("All files converted, unless duplicate gz files exist.\n")
-      return
+        print("All files converted, unless duplicate gz files exist.\n")
+        return
     else:
-      print("%d files remaining"%total_files)
-
+        print("%d files remaining" % total_files)
 
     # Iterating over each file
     counter = 0
     for filename in fits_list:
-      filename = os.path.basename(filename)
-      if not os.path.exists(gz_folder+filename[:-5]+".gz"):
+        filename = os.path.basename(filename)
 
-        img_map = sunpy.map.Map(fits_folder+filename)
-        mean_data = mean_pool(img_map.data,down_sample_factor)
+        if not os.path.exists(gz_folder+filename[:-5]+".gz"):
+            img_map = sunpy.map.Map(fits_folder+filename)
+            mean_data = mean_pool(img_map.data, down_sample_factor)
 
-        if np.isnan(mean_data).any() or np.isinf(mean_data).any() or np.median(mean_data)<=1:
-          print("bad image")
-        else:
-          np.savetxt(X=normalize_exp(mean_data, plot=plot),
-                     fname=gz_folder+filename[:-5]+".gz")
-          if counter%5 == 0:
-            print("%.3f %% finished"%(100.0*counter/total_files))
-          counter += 1
+            if np.isnan(mean_data).any() or np.isinf(mean_data).any() or \
+                    np.median(mean_data) <= 1:
+                print("bad image")
+            else:
+                np.savetxt(X=normalize_exp(mean_data, plot=plot),
+                           fname=gz_folder+filename[:-5]+".gz")
+                if counter % 5 == 0:
+                    print("%.3f %% finished" % (100.0*counter/total_files))
+                counter += 1
+
 
 def normalize_exp(img_array, plot=False):
-  """
-  Normalizes image arrays to a range of 0 to 1, based on the normalization
-  equation 1-exp(-image/factor), where the factor term is selected such that
-  the image is not highly skewed towards values of ~0 or ~1.
+    """
+    Normalizes image arrays to a range of 0 to 1, based on the normalization
+    equation 1-exp(-image/factor), where the factor term is selected such that
+    the image is not highly skewed towards values of ~0 or ~1.
 
-  Inputs
-  ------
-    img_array : Numpy array which contains the pixel values for some image,
-                which is to be normalized based on the 1-exp(-x) equations
+    Inputs
+    ------
+      img_array : Numpy array which contains the pixel values for some image,
+                  which is to be normalized based on the 1-exp(-x) equations
 
-  Outputs
-  -------
-    The normalized array ranging from 0 to 1
-  """
-  if plot:
-    plt.imshow(img_array)
+    Outputs
+    -------
+      The normalized array ranging from 0 to 1
+    """
+    if plot:
+        plt.imshow(img_array)
+        plt.show()
+    img_array[img_array < 0] = 0
+
+    norm_img = 1-np.exp(-img_array/100/np.median(img_array))
+    # print(np.median(img_array))
+    if plot:
+        plt.imshow(norm_img, vmin=0, vmax=1)
+        plt.colorbar()
     plt.show()
-  img_array[img_array<0] = 0
+    return norm_img
 
-  norm_img = 1-np.exp(-img_array/100/np.median(img_array))
-  #print(np.median(img_array))
-  if plot:
-    plt.imshow(norm_img,vmin=0,vmax=1)
-    plt.colorbar()
-  plt.show()
-  return norm_img
 
-##### Script
-AIA_path = "/content/drive/Shareddrives/Phys 477 - Astro Project/AIA_files/"
-event_folder = 'XRA_events/'
-null_folder = 'XRA_nulls/'
+# =============================================================================
+#                                  Main Function
+# =============================================================================
+def reduce_data(wavelengths, date_paths, reduction_factor=8, event="XRA"):
+    """
+    This function reduces full size (4096x4096) .fits images, downscales them,
+        and then uploads them back into the google drive as .gz files.
 
-wavelengths = ([131])
-#date_paths = ["2015_01_","2015_02_","2015_03_","2015_04_","2015_05_","2015_06_",
-#              "2015_07_","2015_08_","2015_09_","2015_10_","2015_11_","2015_12_"]
-#date_paths = ["2015_07_","2015_08_","2015_09_","2015_10_","2015_11_"]
-date_paths = ["2011_01_", "2014_01_", "2016_02_"]
+    Parameters
+    ----------
+    wavelengths : 1-D list of integers
+        List of wavelengths over which to reduce the data.
+        For example: [131, 171, 211]
+    date_paths : 1-D list of Strings
+        List of years and months over which to reduce the data.
+        Each string is given in the format "[year]_[month]_"
+        For example: ["2012_03_", "2014_12_"]
+    reduction_factor : Integer, optional
+        Factor by which to reduce the original 4096 x 4096 images.
+        For example: reduction_factor = 8 produces 512 x 512 images.
+        The default is 8.
+    event : String, optional
+        The type of event according to the event designations from the space
+        weather reports. The default is "XRA". Possible values include "FLA".
 
-for w in wavelengths:
-  for date in date_paths:
-    event_path = AIA_path + event_folder + date + str(w) +"/"
-    null_path = AIA_path + null_folder + date + str(w) +"/"
+    Returns
+    -------
+    Success : Boolean
+        Returns whether the function succeeded (True) or failed (False).
+    """
+    # Check for bad arguments
+    # Event check
+    possible_events = ["XRA", "FLA"]
+    event_is_valid = False
+    for possible_event in possible_events:
+        if event == possible_event:
+            event_is_valid = True
+            break
+    if not event_is_valid:
+        print("Event Type '" + event + "' is invalid")
+        return False
 
-    event_gz_path = AIA_path + event_folder + "gz_" + str(w) +"_512/"
-    null_gz_path = AIA_path + null_folder + "gz_" + str(w) +"_512/"
+    # Check for bad reduction factor
+    if 4096 % reduction_factor:
+        print("Reduction Factor '" + str() + "' is invalid for a 4096 x 4096"
+              + " image")
+        return False
 
-    print("Converting path: " + event_folder[52:] + date + str(w) +"/ to gz_512")
-    convert_to_gz(event_path, event_gz_path, 8, date[:-1], plot=False)
-    print("Converting path: " + null_folder[52:] + date + str(w) +"/ to gz_512")
-    convert_to_gz(null_path, null_gz_path, 8, date[:-1], plot=False)
-print("\nAll Conversions Complete.")
+    AIA_path = "/content/drive/Shareddrives/Phys 477 - Astro Project/" \
+               + "AIA_files/"
+    event_folder = event + '_events/'
+    null_folder = event + '_nulls/'
 
+    # wavelengths = ([131])
+    # date_paths = ["2011_01_", "2014_01_", "2016_02_"]
+
+    # Iterate over all combinations of dates and wavelengths
+    for w in wavelengths:
+        for date in date_paths:
+            event_path = AIA_path + event_folder + date + str(w) + "/"
+            null_path = AIA_path + null_folder + date + str(w) + "/"
+
+            event_gz_path = AIA_path + event_folder + "gz_" + str(w) + "_512/"
+            null_gz_path = AIA_path + null_folder + "gz_" + str(w) + "_512/"
+
+            print("Converting path: " + event_folder[52:] + date + str(w)
+                  + "/ to gz_512")
+            convert_to_gz(event_path, event_gz_path, 8, date[:-1], plot=False)
+
+            print("Converting path: " + null_folder[52:] + date + str(w)
+                  + "/ to gz_512")
+            convert_to_gz(null_path, null_gz_path, 8, date[:-1], plot=False)
+
+    print("\nAll Conversions Complete.")
+    return True

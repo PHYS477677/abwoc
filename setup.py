dcf885fa2d00fe11d011ef510ba73872a9627f65
tgrosson <54158956+tgrosson@users.noreply.github.com>
2021-04-16 16:13:18 -0500

    fixed setup.py error?
    maybe

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ from distutils.core import setup
 setup(name='Astros but Without Cheating',
       version='0.1',
       author='Grosson, Meng, Tracy, White',
-      packages=['abwoc'],
+      packages=['abwoc']
      )

c3d9b7ee2288228ded6de58dd7c43bf840cbe03c
tgrosson <54158956+tgrosson@users.noreply.github.com>
2021-04-16 15:18:12 -0500

    Structured as package

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Astros but Without Cheating',
+      version='0.1',
+      author='Grosson, Meng, Tracy, White',
+      packages=['abwoc'],
+     )

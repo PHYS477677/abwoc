c35039848188d9aea16cb72e6213282c1a11c981
tgrosson <54158956+tgrosson@users.noreply.github.com>
2021-04-26 14:33:34 -0500

    Updated visualization

---
+++ abwoc/visualization.py
@@ -0,0 +1,118 @@
+'''
+visualization.py
+
+Collection of functions to display images of the Sun.
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import ticker
+from sunpy.visualization import axis_labels_from_ctype
+import astropy.units as u
+from astropy.coordinates import SkyCoord
+import sunpy.map
+import warnings
+
+
+def add_plots(smaps, fig, ax):
+    '''
+    Add plots to a figure.
+    
+    Inputs:
+        smaps: list of SunPy Map objects
+        fig: matplotlib Figure object
+        ax: flattened list of matplotlib Axis objects
+    '''
+
+    #ignore warnings for convenience
+    warnings.filterwarnings("ignore")
+    
+    for i in range(len(smaps)):
+        smaps[i].plot(axes=ax[i], annotate=False)
+        ax[i].set_title(smaps[i].wavelength)
+
+        cb = plt.colorbar()
+        tick_locator = ticker.MaxNLocator(nbins=5)
+        cb.locator = tick_locator
+        cb.update_ticks()
+
+
+def plots(smaps):
+    '''
+    Plots an image of the Sun in multiple wavelengths. Can display
+    up to nine images in one figure.
+
+    Inputs:
+        smaps: list of SunPy Map objects (up to nine)
+    '''
+
+    nmap = len(smaps)
+
+    if (nmap<1 or nmap>9):
+        raise Exception('Please input a valid number of smaps.')
+
+    nc = int(np.ceil(np.sqrt(nmap)))
+    nr = nc - 1*(nmap<=(nc**2-nc))
+
+    fig, ax = plt.subplots(nr, nc, sharex=True, sharey=True,
+                           figsize=(2*nc+2,2*nr+1))
+    if nmap==1:
+        ax = np.array([ax])
+        fig.set_size_inches(5, 4.5) #prevent text overlap
+
+    ax = ax.reshape(-1)
+
+    #hide extra axes
+    for i in range(nmap,nr*nc):
+        ax[i].axis('off')
+
+    add_plots(smaps, fig, ax)
+
+    #share axis labels over the whole figure
+    ylab = axis_labels_from_ctype(smap[0].coordinate_system[1],
+                                            smap[0].spatial_units[1])
+    xlab = axis_labels_from_ctype(smap[0].coordinate_system[0],
+                                            smap[0].spatial_units[0])
+    fig.text(0.5, 0.04, xlab, ha='center')
+    fig.text(0.01, 0.5, ylab, va='center', rotation='vertical')
+
+    title = 'AIA ' + smaps[0].meta.get('date').replace('T',' ')[:-3]
+    plt.suptitle(title)
+
+    plt.show()
+
+
+def plot_zoom(smaps, x0, y0, xlength, ylength):
+    '''
+    Plots an image of the Sun zoomed in on a region of interest.
+    Location of area (in helioprojective arcsec) must be known.
+
+    Inputs:
+        smaps: list of SunPy Map objects (up to nine)
+        x0: bottom left corner of zoom area (helioprojective longitude
+            - arcsec)
+        y0: bottom left corner of zoom area (helioprojective lattitude
+            - arcsec)
+        xlength, ylength: width and height of zoom area (arcsec)
+    '''
+    x0 *= u.arcsec
+    y0 *= u.arcsec
+    xlength *= u.arcsec
+    ylength *= u.arcsec
+
+    nmap = len(smaps)
+    submaps = [0]*nmap
+    for i in range(nmap):
+        bottom_left = SkyCoord(x0, y0, frame=smap[i].coordinate_frame)
+        top_right = SkyCoord(x0 + xlength, y0 + ylength,
+                             frame = smap[i].coordinate_frame)
+        submaps[i] = smap[i].submap(bottom_left, top_right=top_right)
+    plots(submaps)
+
+
+
+
+
+
+
+

f1b235ad6eb3b8cbe8630653529dcc0b03b18ab4
JacksonRW <59513752+JacksonRW@users.noreply.github.com>
2021-04-26 22:03:05 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,6 @@ setup(name='abwoc',
       author='Grosson, Meng, Tracy, White',
       packages=find_packages(),
       include_package_data = True,
-      package_data={"abwoc": ["abwoc/*.txt"]},
+      package_data={"abwoc": ["*.txt"]},
       install_requires=requires
      )

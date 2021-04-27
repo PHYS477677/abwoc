87829d70cce624c1ffe4fe947d4083db5f95c373
therealcmeng <78132136+therealcmeng@users.noreply.github.com>
2021-04-27 16:21:17 -0500

    Pep-8 formatting of visualization.py

--- abwoc/visualization.py
+++ abwoc/visualization.py
@@ -1,8 +1,8 @@
-'''
-visualization.py
+"""
+visualization.py.
 
 Collection of functions to display images of the Sun.
-'''
+"""
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,23 +10,21 @@ from matplotlib import ticker
 from sunpy.visualization import axis_labels_from_ctype
 import astropy.units as u
 from astropy.coordinates import SkyCoord
-import sunpy.map
 import warnings
 
 
 def add_plots(smaps, fig, ax):
-    '''
+    """
     Add plots to a figure.
-    
+
     Inputs:
         smaps: list of SunPy Map objects
         fig: matplotlib Figure object
         ax: flattened list of matplotlib Axis objects
-    '''
-
-    #ignore warnings for convenience
+    """
+    # ignore warnings for convenience
     warnings.filterwarnings("ignore")
-    
+
     for i in range(len(smaps)):
         smaps[i].plot(axes=ax[i], annotate=False)
         ax[i].set_title(smaps[i].wavelength)
@@ -35,56 +33,60 @@ def add_plots(smaps, fig, ax):
         tick_locator = ticker.MaxNLocator(nbins=5)
         cb.locator = tick_locator
         cb.update_ticks()
+    return
 
 
 def plots(smaps):
-    '''
-    Plots an image of the Sun in multiple wavelengths. Can display
-    up to nine images in one figure.
+    """
+    Plot an image of the Sun in multiple wavelengths, up to nine per figure.
 
     Inputs:
         smaps: list of SunPy Map objects (up to nine)
-    '''
 
+    Outputs:
+        None.
+    """
     nmap = len(smaps)
 
-    if (nmap<1 or nmap>9):
+    if (nmap < 1 or nmap > 9):
         raise Exception('Please input a valid number of smaps.')
 
     nc = int(np.ceil(np.sqrt(nmap)))
-    nr = nc - 1*(nmap<=(nc**2-nc))
+    nr = nc - 1*(nmap <= (nc**2 - nc))
 
     fig, ax = plt.subplots(nr, nc, sharex=True, sharey=True,
-                           figsize=(2*nc+2,2*nr+1))
-    if nmap==1:
+                           figsize=(2*nc+2, 2*nr+1))
+    if nmap == 1:
         ax = np.array([ax])
-        fig.set_size_inches(5, 4.5) #prevent text overlap
+        fig.set_size_inches(5, 4.5)    # prevent text overlap
 
     ax = ax.reshape(-1)
 
-    #hide extra axes
-    for i in range(nmap,nr*nc):
+    # hide extra axes
+    for i in range(nmap, nr*nc):
         ax[i].axis('off')
 
     add_plots(smaps, fig, ax)
 
-    #share axis labels over the whole figure
+    # share axis labels over the whole figure
     ylab = axis_labels_from_ctype(smaps[0].coordinate_system[1],
-                                            smaps[0].spatial_units[1])
+                                  smaps[0].spatial_units[1])
     xlab = axis_labels_from_ctype(smaps[0].coordinate_system[0],
-                                            smaps[0].spatial_units[0])
+                                  smaps[0].spatial_units[0])
     fig.text(0.5, 0.04, xlab, ha='center')
     fig.text(0.01, 0.5, ylab, va='center', rotation='vertical')
 
-    title = 'AIA ' + smaps[0].meta.get('date').replace('T',' ')[:-3]
+    title = 'AIA ' + smaps[0].meta.get('date').replace('T', ' ')[:-3]
     plt.suptitle(title)
 
     plt.show()
+    return
 
 
 def plot_zoom(smaps, x0, y0, xlength, ylength):
-    '''
-    Plots an image of the Sun zoomed in on a region of interest.
+    """
+    Plot an image of the Sun zoomed in on a region of interest.
+
     Location of area (in helioprojective arcsec) must be known.
 
     Inputs:
@@ -94,7 +96,10 @@ def plot_zoom(smaps, x0, y0, xlength, ylength):
         y0: bottom left corner of zoom area (helioprojective lattitude
             - arcsec)
         xlength, ylength: width and height of zoom area (arcsec)
-    '''
+
+    Outputs:
+        None.
+    """
     x0 *= u.arcsec
     y0 *= u.arcsec
     xlength *= u.arcsec
@@ -105,14 +110,7 @@ def plot_zoom(smaps, x0, y0, xlength, ylength):
     for i in range(nmap):
         bottom_left = SkyCoord(x0, y0, frame=smaps[i].coordinate_frame)
         top_right = SkyCoord(x0 + xlength, y0 + ylength,
-                             frame = smaps[i].coordinate_frame)
+                             frame=smaps[i].coordinate_frame)
         submaps[i] = smaps[i].submap(bottom_left, top_right=top_right)
     plots(submaps)
-
-
-
-
-
-
-
-
+    return
